d93c9b24599f1145824f977bbc88e5e8806c7ea1
Makar Bobukh <40933190+Raleyph@users.noreply.github.com>
2022-02-03 12:05:23 +0200

    Alpha 0.6.2

--- Assets/Rooms/RoomBehaviour.cs
+++ Assets/Rooms/RoomBehaviour.cs
@@ -8,9 +8,7 @@ public class RoomBehaviour : MonoBehaviour {
     public GameObject[] walls; // 0 - Up 1 - Down 2 - Right 3 - Left
     public GameObject[] doors;
     private GameObject RoomName;
-    
-    private Spawner Spawner;
-    
+
     public string NameRoom;
 
     private void Awake() {
@@ -18,8 +16,6 @@ public class RoomBehaviour : MonoBehaviour {
     }
 
     public void UpdateRoom(bool[] status) {
-        Spawner = GameObject.FindObjectOfType<Spawner>();
-        
         for (int i = 0; i < status.Length; i++) {
             doors[i].SetActive(status[i]);
             walls[i].SetActive(!status[i]);
@@ -31,12 +27,4 @@ public class RoomBehaviour : MonoBehaviour {
             RoomName.GetComponent<Text>().text = NameRoom;
         }
     }
-/*
-    private void OnTriggerStay(Collider other) {
-        if (other.tag == "Player") Spawner.DoSpawn();
-    }
-
-    private void OnTriggerExit(Collider other) {
-        throw new NotImplementedException();
-    }*/
 }
