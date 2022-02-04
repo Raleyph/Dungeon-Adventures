445bbc7c4bb9eca969f763ce270abc788de985f9
Makar Bobukh <40933190+Raleyph@users.noreply.github.com>
2022-02-04 21:49:14 +0200

    Alpha 0.6.4

--- Assets/Characters/Skeleton/Skeleton.cs
+++ Assets/Characters/Skeleton/Skeleton.cs
@@ -8,26 +8,31 @@ public class Skeleton : MonoBehaviour {
     private GameObject Sam;
     
     public float EnemySpeed;
-    public float SpeedBoost;
     public int health = 10;
     public bool isActive;
 
+    private Animator Anim;
+
     private void Start() {
         Sam = GameObject.FindWithTag("Player");
+        Anim = GetComponent<Animator>();
     }
 
     void FixedUpdate() {
         if (isActive) {
             float dist = Vector3.Distance(Sam.transform.position, transform.position);
 
-            if (dist <= 10) {
+            if (dist <= 20) {
                 Zentr = Sam.transform.position;
                 Zentr.y = 0;
-                Vector3 EnemyMove = Vector3.MoveTowards(transform.position, Zentr, EnemySpeed * SpeedBoost * Time.deltaTime);
-        
+                Vector3 EnemyMove = Vector3.MoveTowards(transform.position, Zentr, EnemySpeed * Time.deltaTime);
+
                 transform.position = new Vector3(EnemyMove.x, EnemyMove.y, EnemyMove.z);
                 transform.LookAt(Zentr);
-            }   
+                Anim.SetBool("Move", true);
+            } else {
+                Anim.SetBool("Move", false);
+            }
         }
     }
     
--- Assets/Rooms/RoomBehaviour.cs
+++ Assets/Rooms/RoomBehaviour.cs
@@ -3,13 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class RoomBehaviour : MonoBehaviour {
     public GameObject[] walls; // 0 - Up 1 - Down 2 - Right 3 - Left
     public GameObject[] doors;
+    public GameObject[] Spawners;
+    public GameObject[] Enemy;
     private GameObject RoomName;
 
     public string NameRoom;
+    public int SpawnerType;
+    public bool HaveSpawner;
 
     private void Awake() {
         RoomName = GameObject.FindWithTag("Room Name");
@@ -26,7 +31,23 @@ public class RoomBehaviour : MonoBehaviour {
         if (Time.timeScale == 1) {
             if (other.tag == "Player") {
                 RoomName.GetComponent<Text>().text = NameRoom;
+                if (HaveSpawner) {
+                    StartCoroutine(Spawn());
+                }
             }
         }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (Time.timeScale == 1) {
+            if (other.tag == "Player" && HaveSpawner) {
+                StopCoroutine(Spawn());
+            }
+        }
+    }
+
+    IEnumerator Spawn() {
+        Instantiate(Enemy[SpawnerType], Spawners[Random.Range(0, Spawners.Length)].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(1f);
     }
 }
